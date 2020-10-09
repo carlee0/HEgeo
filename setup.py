d527f4e319de8a4a733eaa777190468feb051a3b
Carmen Lee <10846449+carlee0@users.noreply.github.com>
2020-10-09 09:15:59 +0200

    Update setup.py
    update url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Geo fencing using homomorphic encryption",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/CarmenLee111/HEgeo",
+    url="https://github.com/carlee0/HEgeo",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
